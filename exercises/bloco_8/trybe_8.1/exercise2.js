011d1a8c5f71b5945ce222efb1e09a7dc152285c
Igor Nascimento <igorln96@gmail.com>
2020-11-30 18:08:15 -0300

    exercicio 2

--- exercises/bloco_8/trybe_8.1/exercise2.js
+++ exercises/bloco_8/trybe_8.1/exercise2.js
@@ -63,9 +63,15 @@ const books = [
   },
 ];
 
-function authorBornIn1947() {
+function smallerName() {
+  let nameBook;
   // escreva aqui o seu código
-  return books.find(book => book.author.birthYear === 1947).author.name;
+  books.forEach(book => {
+    if (!nameBook || book.name.length < nameBook.length){
+      nameBook = book.name;
+    }})
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
 }
 
-assert.equal(authorBornIn1947(), 'Stephen King');
+assert.equal(smallerName(), 'Duna');
